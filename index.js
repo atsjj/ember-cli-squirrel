e018e32abb0d2cd3915c770602065a54a971a1a2
Steve Jabour <steve@jabour.me>
2015-10-22 14:32:51 -0600

    Added project files from prior in-repo-addon.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 /* jshint node: true */
 'use strict';
 
+var commands = require('./lib/commands');
+
 module.exports = {
-  name: 'ember-cli-squirrel'
+  name: 'ember-cli-squirrel',
+
+  includedCommands: function() {
+    return commands;
+  }
 };
